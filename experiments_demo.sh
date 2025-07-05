15e9d62baa5e456fe7268e40b7271d3add5c49b6
Jiayao Yang <101309708+jyang000@users.noreply.github.com>
2025-07-05 17:20:14 -0400

    Delete experiments_demo.sh

--- experiments_demo.sh
+++
@@ -1,8 +0,0 @@
-echo 'REFOCUSING PULSE DESIGN'
-python demo_refocusing.py --b0map 1 --b1map 1 --folder refocusing_demo --optimization 1
-python demo_refocusing.py --b0map 1 --b1map 1 --folder refocusing_demo --evaluation 1
-
-
-echo 'EXCITATION PULSE DESIGN'
-python demo_excitation.py --b0map 1 --b1map 1 --folder excitation_demo --optimization 1
-python demo_excitation.py --b0map 1 --b1map 1 --folder excitation_demo --evaluation 1
